5ac50d1cdc80e0cb27e8b1376272c92abc66bbd5
zafarivaev <z.ivaev@mail.ru>
2020-01-24 12:30:36 +0500

    Initial commit

--- WKWebView/ViewController.swift
+++ WKWebView/ViewController.swift
@@ -7,14 +7,80 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKUIDelegate {
+    
+    // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
+        setupNavItem()
+        let myURL = URL(string: "https://www.apple.com")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setupNavBar()
+    }
+    
+    // MARK: - Actions
+    @objc func forwardAction() {
+        if webView.canGoForward {
+            webView.goForward()
+        }
+    }
+    
+    @objc func backAction() {
+        if webView.canGoBack {
+            webView.goBack()
+        }
+    }
+    
+    // MARK: - Properties
+    lazy var webView: WKWebView = {
+        let webConfiguration = WKWebViewConfiguration()
+        let webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        return webView
+    }()
+    
+    let forwardBarItem = UIBarButtonItem(title: "Forward", style: .plain, target: self, action: #selector(forwardAction))
+    
+    let backBarItem = UIBarButtonItem(title: "Backward", style: .plain, target: self, action: #selector(backAction))
+    
+}
 
-
+extension ViewController {
+    func setupUI() {
+        self.view.backgroundColor = .white
+        self.view.addSubview(webView)
+        
+        NSLayoutConstraint.activate([
+            webView.topAnchor
+                .constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            webView.leftAnchor
+                .constraint(equalTo: self.view.safeAreaLayoutGuide.leftAnchor),
+            webView.bottomAnchor
+                .constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
+            webView.rightAnchor
+                .constraint(equalTo: self.view.safeAreaLayoutGuide.rightAnchor)
+        ])
+    }
+    
+    func setupNavItem() {
+        self.navigationItem.leftBarButtonItem = backBarItem
+        self.navigationItem.rightBarButtonItem = forwardBarItem
+    }
+    
+    func setupNavBar() {
+        self.navigationController?.navigationBar
+            .barTintColor = .systemBlue
+        self.navigationController?.navigationBar
+            .tintColor = .white
+    }
 }
 
